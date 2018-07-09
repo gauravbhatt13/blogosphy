5987082669b4e2d4a1f5d339d92644bc5c86bf1b
gauravbhatt13 <gauravbhatt13@gmail.com>
2018-07-10 00:17:19 +0530

    updated

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.use('/blogs/login', loginRouter);
 app.use('/blogs/category', blogsCategoryRouter);
 app.use('/blogs', blogsRouter);
 app.use(express.static(path.join(__dirname, 'app')));
-app.use(express.static(path.join(__dirname, 'app/app')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 module.exports = app;
